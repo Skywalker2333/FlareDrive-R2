28ae87eeb6e45717d467cc272b87e275722a531f
cambo Guo <76270078+Skywalker2333@users.noreply.github.com>
2025-06-15 12:46:38 +0800

    Update Footer.vue

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -22,8 +22,8 @@
     <div class="project-links">
       <strong>项目:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
         rel="noopener noreferrer">FlareDrive-R2</a>
-      <strong>作者:</strong><a href="https://www.goujam.fun" target="_blank"
-        rel="noopener noreferrer">LiuShen</a>
+      <strong>服主:</strong><a href="https://www.goujam.fun" target="_blank"
+        rel="noopener noreferrer">guojam</a>
       <strong>初始项目:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
         rel="noopener noreferrer">Cloudflare-R2-OSS</a>
     </div>
@@ -40,10 +40,10 @@ export default {
   name: "Footer",
   data() {
     return {
-      homeUrl: "https://www.liushen.fun/",
-      blogUrl: "https://blog.liushen.fun/",
-      githubUrl: "https://github.com/willow-god",
-      emailUrl: "mailto:01@liushen.fun"
+      homeUrl: "https://cloud.guojam.fun/",
+      blogUrl: "https://www.guojam.fun/",
+      githubUrl: "https://github.com/Skywalker2333",
+      emailUrl: "mailto:guojam@guojam.fun"
     };
   }
 };
